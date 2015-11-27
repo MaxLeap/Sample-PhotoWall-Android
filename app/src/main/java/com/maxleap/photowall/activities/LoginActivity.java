cb807333de8c9cd274581079089a1dc95e185dcb
sidneyy <siriuseddy@gmail.com>
2015-11-27 17:05:35 +0800

    Update.

--- app/src/main/java/com/maxleap/photowall/activities/LoginActivity.java
+++ app/src/main/java/com/maxleap/photowall/activities/LoginActivity.java
@@ -8,7 +8,12 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.maxleap.*;
+
+import com.maxleap.LogInCallback;
+import com.maxleap.MLLog;
+import com.maxleap.MLUser;
+import com.maxleap.MLUserManager;
+import com.maxleap.SignUpCallback;
 import com.maxleap.exception.MLException;
 import com.maxleap.photowall.BaseActivity;
 import com.maxleap.photowall.R;
@@ -17,10 +22,10 @@ public class LoginActivity extends BaseActivity {
 
     public static final String TAG = LoginActivity.class.getName();
 
-    private EditText emailEditText;
-    private EditText passwordEditText;
-    private TextInputLayout emailTextInputLayout;
-    private TextInputLayout passwordTextInputLayout;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private TextInputLayout mEmailTextInputLayout;
+    private TextInputLayout mPasswordTextInputLayout;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -35,19 +40,19 @@ public class LoginActivity extends BaseActivity {
 
         setUpToolbar();
         mActionBar.setDisplayHomeAsUpEnabled(false);
-        emailEditText = (EditText) findViewById(R.id.email_edit_text);
-        emailTextInputLayout = (TextInputLayout) findViewById(R.id.email_input_layout);
-        passwordEditText = (EditText) findViewById(R.id.password_edit_text);
-        passwordTextInputLayout = (TextInputLayout) findViewById(R.id.password_input_layout);
-        emailTextInputLayout.setErrorEnabled(true);
-        passwordTextInputLayout.setErrorEnabled(true);
+        mEmailEditText = (EditText) findViewById(R.id.email_edit_text);
+        mEmailTextInputLayout = (TextInputLayout) findViewById(R.id.email_input_layout);
+        mPasswordEditText = (EditText) findViewById(R.id.password_edit_text);
+        mPasswordTextInputLayout = (TextInputLayout) findViewById(R.id.password_input_layout);
+        mEmailTextInputLayout.setErrorEnabled(true);
+        mPasswordTextInputLayout.setErrorEnabled(true);
 
         findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 if (validate()) {
-                    MLUserManager.logInInBackground(emailEditText.getText().toString(),
-                            passwordEditText.getText().toString(),
+                    MLUserManager.logInInBackground(mEmailEditText.getText().toString(),
+                            mPasswordEditText.getText().toString(),
                             new LogInCallback<MLUser>() {
                                 @Override
                                 public void done(final MLUser mlUser, final MLException e) {
@@ -69,8 +74,8 @@ public class LoginActivity extends BaseActivity {
             public void onClick(final View v) {
                 if (validate()) {
                     MLUser user = new MLUser();
-                    user.setUserName(emailEditText.getText().toString());
-                    user.setPassword(passwordEditText.getText().toString());
+                    user.setUserName(mEmailEditText.getText().toString());
+                    user.setPassword(mPasswordEditText.getText().toString());
                     MLUserManager.signUpInBackground(user, new SignUpCallback() {
                         @Override
                         public void done(final MLException e) {
@@ -95,18 +100,18 @@ public class LoginActivity extends BaseActivity {
     }
 
     private boolean validate() {
-        emailTextInputLayout.setError("");
-        passwordTextInputLayout.setError("");
-        if (TextUtils.isEmpty(emailEditText.getText().toString())) {
-            emailTextInputLayout.setError(getString(R.string.err_require_email));
+        mEmailTextInputLayout.setError("");
+        mPasswordTextInputLayout.setError("");
+        if (TextUtils.isEmpty(mEmailEditText.getText().toString())) {
+            mEmailTextInputLayout.setError(getString(R.string.err_require_email));
             return false;
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(emailEditText.getText().toString()).matches()) {
-            emailTextInputLayout.setError(getString(R.string.err_invalid_email));
+        if (!Patterns.EMAIL_ADDRESS.matcher(mEmailEditText.getText().toString()).matches()) {
+            mEmailTextInputLayout.setError(getString(R.string.err_invalid_email));
             return false;
         }
-        if (TextUtils.isEmpty(passwordEditText.getText().toString())) {
-            passwordTextInputLayout.setError(getString(R.string.err_require_password));
+        if (TextUtils.isEmpty(mPasswordEditText.getText().toString())) {
+            mPasswordTextInputLayout.setError(getString(R.string.err_require_password));
             return false;
         }
         return true;
